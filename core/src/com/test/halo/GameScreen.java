ab0d102c8eefb180ab6d99c2dcae58734265f022
unknown <nathanielmorihara@gmail.com>
2016-02-21 06:50:27 +0000

    First commit

---
+++ core/src/com/test/halo/GameScreen.java
@@ -0,0 +1,39 @@
+package com.test.halo;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Nathaniel on 2/20/2016.
+ */
+public class GameScreen implements Screen {
+
+    final Halo game;
+
+    public GameScreen(final Halo game){
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height){}
+
+    @Override
+    public void show(){}
+
+    @Override
+    public void hide(){}
+
+    @Override
+    public void pause(){}
+
+    @Override
+    public void resume(){}
+
+    @Override
+    public void dispose(){}
+}
